78e7bc171257087d2be22250454cb7d28a9b3335
Владислав Пономарев <Public@PonomareVlad.ru>
2018-04-25 13:02:50 +0500

    Select arrow

--- assets/js/mask.js
+++ assets/js/mask.js
@@ -1,5 +1,6 @@
-function mask(event) {
-    event.target.value = event.target.value.replace(/[^a-z0-9]+/gi, '').replace(/(.{3})/g, '$1 ');
+function mask(event, node) {
+    node = node ? node : event.target;
+    node.value = node.value.replace(/[^a-z0-9]+/gi, '').replace(/(.{3})/g, '$1 ');
     return true;
 }
 
@@ -7,4 +8,5 @@ document.querySelectorAll('[data-input-mask]').forEach(function (node) {
     node.addEventListener('keypress', mask);
     node.addEventListener('change', mask);
     node.addEventListener('blur', mask);
+    mask(false, node);
 });
