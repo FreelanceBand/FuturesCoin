2c886b5a1273a492fe3f5ea48ef84afcdc4d95c0
Владислав Пономарев <Public@PonomareVlad.ru>
2018-04-25 16:27:43 +0500

    Small fixes

--- assets/js/bet.js
+++ assets/js/bet.js
@@ -21,6 +21,20 @@ document.querySelectorAll('[data-input-mask]').forEach(function (node) {
     mask(false, node);
 });
 
+document.querySelector('.amount .custom-select').addEventListener('click', function () {
+    this.blur();
+    this.querySelectorAll('input:not(:checked) + label').forEach(function (node) {
+        node.style.display = 'none';
+    });
+    let self = this;
+    setTimeout(function () {
+        // self.style.pointerEvents = 'initial';
+        self.querySelectorAll('input:not(:checked) + label').forEach(function (node) {
+            node.style.display = 'none';
+        });
+    }, 100);
+});
+
 function checkTimeValue() {
     let value = parseInt(document.querySelector('#time').value);
     document.querySelector('#timeFall').disabled = value <= 1;
